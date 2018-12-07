b889f9fd4d942316dcb4f23d4c88b95e83408a34
jan bos <max.bos217@gmail.com>
2018-12-07 14:53:23 +0100

    added encryption

--- Encryption/Form1.Designer.cs
+++ Encryption/Form1.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.KeyTextBox = new System.Windows.Forms.TextBox();
+            this.DecryptKey = new System.Windows.Forms.Button();
+            this.EncryptKey = new System.Windows.Forms.Button();
+            this.ChooseFileButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // KeyTextBox
+            // 
+            this.KeyTextBox.Location = new System.Drawing.Point(93, 14);
+            this.KeyTextBox.Name = "KeyTextBox";
+            this.KeyTextBox.Size = new System.Drawing.Size(329, 20);
+            this.KeyTextBox.TabIndex = 2;
+            this.KeyTextBox.Text = "Key";
+            // 
+            // DecryptKey
+            // 
+            this.DecryptKey.Location = new System.Drawing.Point(12, 41);
+            this.DecryptKey.Name = "DecryptKey";
+            this.DecryptKey.Size = new System.Drawing.Size(75, 23);
+            this.DecryptKey.TabIndex = 3;
+            this.DecryptKey.Text = "Decrypt";
+            this.DecryptKey.UseVisualStyleBackColor = true;
+            this.DecryptKey.Click += new System.EventHandler(this.DecryptClick);
+            // 
+            // EncryptKey
+            // 
+            this.EncryptKey.Location = new System.Drawing.Point(12, 12);
+            this.EncryptKey.Name = "EncryptKey";
+            this.EncryptKey.Size = new System.Drawing.Size(75, 23);
+            this.EncryptKey.TabIndex = 4;
+            this.EncryptKey.Text = "Encrypt";
+            this.EncryptKey.UseVisualStyleBackColor = true;
+            this.EncryptKey.Click += new System.EventHandler(this.EncryptClick);
+            // 
+            // ChooseFileButton
+            // 
+            this.ChooseFileButton.Location = new System.Drawing.Point(93, 41);
+            this.ChooseFileButton.Name = "ChooseFileButton";
+            this.ChooseFileButton.Size = new System.Drawing.Size(75, 23);
+            this.ChooseFileButton.TabIndex = 5;
+            this.ChooseFileButton.Text = "Choose File";
+            this.ChooseFileButton.UseVisualStyleBackColor = true;
+            this.ChooseFileButton.Click += new System.EventHandler(this.ChooseFileClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.Location = new System.Drawing.Point(174, 43);
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.ReadOnly = true;
+            this.PathTextBox.Size = new System.Drawing.Size(248, 20);
+            this.PathTextBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 73);
+            this.Controls.Add(this.PathTextBox);
+            this.Controls.Add(this.ChooseFileButton);
+            this.Controls.Add(this.EncryptKey);
+            this.Controls.Add(this.DecryptKey);
+            this.Controls.Add(this.KeyTextBox);
+            this.MaximumSize = new System.Drawing.Size(450, 112);
+            this.MinimumSize = new System.Drawing.Size(450, 112);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox KeyTextBox;
+        private System.Windows.Forms.Button DecryptKey;
+        private System.Windows.Forms.Button EncryptKey;
+        private System.Windows.Forms.Button ChooseFileButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox PathTextBox;
     }
 }
 
--- Encryption/Form1.cs
+++ Encryption/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,113 @@ namespace Encryption
 {
     public partial class Form1 : Form
     {
+
+        string FilePath = "";
+        bool HasFile = false;
+
+        Random rnd = new Random();
+
         public Form1()
         {
+
             InitializeComponent();
         }
+
+        private void EncryptClick(object sender, EventArgs e)
+        {
+            if (HasFile)
+            {
+                string TextToEncrypt = File.ReadAllText(FilePath);
+                char[] Split = TextToEncrypt.ToCharArray();
+                List<byte> Bytes = new List<byte>();
+                foreach (byte b in Encoding.UTF8.GetBytes(Split)) { Bytes.Add(b); };
+
+                char[] FinalChar = new char[Bytes.Count];
+                string Binary = "";
+
+                for (int i = 0; i < Bytes.Count; i++)
+                {
+                    int Direction = rnd.Next(2);
+
+                    Binary += Direction.ToString();
+
+                    Direction = 1 - Direction * 2;
+
+                    FinalChar[i] = Convert.ToChar(Bytes[i]+ Direction);
+                }
+
+                KeyTextBox.Text = Binary;
+
+                using (var tw = new StreamWriter(Path.GetDirectoryName(FilePath)+"\\" + Path.GetFileNameWithoutExtension(FilePath) + "_Encrpyted.txt" , true))
+                {
+                    tw.WriteLine(new string(FinalChar));
+                }
+            }
+        }
+
+        private void DecryptClick(object sender, EventArgs e)
+        {
+            if (HasFile)
+            {
+                char[] Key = KeyTextBox.Text.ToCharArray();
+                Console.WriteLine(Key);
+                string TextToDecrypt = File.ReadAllText(FilePath);
+                char[] Split = TextToDecrypt.ToCharArray();
+
+
+                List<byte> Bytes = new List<byte>();
+                foreach (byte b in Encoding.UTF8.GetBytes(Split)) { Bytes.Add(b); };
+
+                char[] FinalChar = new char[Bytes.Count];
+
+                for (int i = 0; i < Key.Count(); i++)
+                {
+                    int Direction = 1 - int.Parse(Key[i].ToString()) * 2;
+                    FinalChar[i] = Convert.ToChar(Bytes[i] - Direction);
+                }
+
+                using (var tw = new StreamWriter(Path.GetDirectoryName(FilePath) + "\\" + Path.GetFileNameWithoutExtension(FilePath) + "_Decrpyted.txt", true))
+                {
+                    tw.WriteLine(new string(FinalChar));
+                }
+            }
+        }
+        /// <summary>
+        /// get a file and store it's path
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ChooseFileClick(object sender, EventArgs e)
+        {
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                if (Path.GetExtension(file) == ".txt")
+                {
+                    try
+                    {
+                        string text = File.ReadAllText(file);
+                        size = text.Length;
+                        FilePath = file;
+                    }
+                    catch (IOException)
+                    {
+                    }
+                    PathTextBox.Text = FilePath;
+                    HasFile = true;
+
+                }
+                else{
+                    //error not text
+                }
+
+            }
+            
+        }
+
+        
     }
+
 }
